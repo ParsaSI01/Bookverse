ffed6174716fe4bbe68a3447994073aba6b912f7
ParsaSI01 <parsasi0101@email.com>
2026-08-11 17:02:31 +0330

    Improved books\models.py

--- src/books/views.py
+++ src/books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Book
 
-# Create your views here.
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, id=id)
+
+    book.views += 1
+    book.save(update_fields=["views"])
+
+    return render(request, "books/detail.html", {
+        "book": book,
+    })
