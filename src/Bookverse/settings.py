731978ecee0943f623bc2ecc12f824e3c906a4e3
ParsaSI01 <parsasi0101@email.com>
2026-08-11 11:15:32 +0330

    Creating Apps And Adding Then In settings.py And urls.py

--- src/Bookverse/settings.py
+++ src/Bookverse/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'books.apps.BooksConfig',
+    'home.apps.HomeConfig',
+
+
 ]
 
 MIDDLEWARE = [
