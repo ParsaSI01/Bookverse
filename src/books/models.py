5d1b215d1e2f32e923932db359ff3a1e7c56f7dc
rastinsharbatoghli20-dotcom <rastin.sharbatoghli20@gmail.com>
2026-08-11 06:05:15 -0700

    add style.css

--- src/books/models.py
+++ src/books/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
-    ispaid = models.BooleanField(default=False)
+    ispaid = models.BooleanField(default=False)             # parsaSI
     is_published = models.BooleanField(default=False)
     if ispaid:
         price = models.PositiveIntegerField()
